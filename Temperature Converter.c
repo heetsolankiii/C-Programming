22afab78afe2edf8489d20102f4ac4f82037d86d
Heet Solanki <154593803+heetsolankiii@users.noreply.github.com>
2024-01-18 10:57:42 +0530

    Added functions to separate  each unit

--- Temperature Converter.c
+++ Temperature Converter.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+void Celsius(float temperature);    // Declared Function Celsius
+void Farenheit(float temperature);    // Declared Function Farenheit
+void Kelvin(float temperature);    // Declared Function Kelvin
 void main()
 {
-    float temperature,answer=0;
-    char unitFrom,unitTo;
+    float temperature;
+    char unitFrom;
     char c,C,f,F,k,K;
     int start;
     printf("--x--Welcome to TEMPERATURE CONVERTER!--x--\n\n");  //Start of program
@@ -20,9 +23,37 @@ void main()
         scanf("%s",&unitFrom);
         if(unitFrom=='c' || unitFrom=='C')  //It converts from Celsius to other units
         {
-            printf("To which unit: ");
-            scanf("%s",&unitTo);
-            if(unitTo=='f' || unitTo=='F')  //It converts to Farenheit
+            Celsius(temperature);    //Calling Celsius Function
+        }
+        else if(unitFrom=='f' || unitFrom=='F')  //It converts from Farenheit to other units
+        {
+            Farenheit(temperature);    //Calling Farenheit Function
+        }
+        else if(unitFrom=='k' || unitFrom=='K')  //It converts from Kelvin to other units
+        {
+            Kelvin(temperature);    //Calling Kelvin Function
+        }
+        else 
+        {
+            printf("Wrong unit entered.\n");    //If the unit entered is other than mentioned above
+        }
+        printf("To continue enter '1' to STOP enter '0': ");    //To continue or stop the loop
+        scanf("%d",&start);
+    }
+    if(start==0)
+    {
+        printf("\n\n--x--Thanks for using our TEMPERATURE CONVERTER!--x--");    //End of program
+    }
+}
+
+// Function Celsius
+void Celsius(float temperature)
+{
+    int answer=0;
+    char unitTo;
+    printf("To which unit: ");
+    scanf("%s",&unitTo);
+    if(unitTo=='f' || unitTo=='F')  //It converts to Farenheit
             {
                 answer=(temperature*1.8)+32;    //Formula to convert into Farenheit
                 printf("%0.2f C = %0.2f F\n",temperature,answer);
@@ -36,12 +67,16 @@ void main()
             {
                 printf("Wrong unit entered.\n");    //If the unit entered is other than mentioned above
             }
-        }
-        else if(unitFrom=='f' || unitFrom=='F')  //It converts from Farenheit to other units
-        {
-            printf("To which unit: ");
-            scanf("%s",&unitTo);
-            if(unitTo=='c' || unitTo=='C') //It converts to Celsius
+}
+
+// Function Farenheit
+void Farenheit(float temperature)
+{
+    int answer=0;
+    char unitTo;
+    printf("To which unit: ");
+    scanf("%s",&unitTo);
+    if(unitTo=='c' || unitTo=='C') //It converts to Celsius
             {
                 answer=(temperature-32)*0.55;  //Formula to convert into Celsius
                 printf("%0.2f F = %0.2f C\n",temperature,answer);
@@ -55,12 +90,16 @@ void main()
             {
                 printf("Wrong unit entered.\n");    //If the unit entered is other than mentioned above
             }
-        }
-        else if(unitFrom=='k' || unitFrom=='K')  //It converts from Kelvin to other units
-        {
-            printf("To which unit: ");
-            scanf("%s",&unitTo);
-            if(unitTo=='c' || unitTo=='C') //It converts to Celsius
+}
+
+// Function Kelvin
+void Kelvin(float temperature)
+{
+    int answer=0;
+    char unitTo;
+    printf("To which unit: ");
+    scanf("%s",&unitTo);
+    if(unitTo=='c' || unitTo=='C') //It converts to Celsius
             {
                 answer=temperature-273.15;  //Formula to convert into Celsius
                 printf("%0.2f K = %0.2f C\n",temperature,answer);
@@ -74,17 +113,4 @@ void main()
             {
                 printf("Wrong unit entered.\n");    //If the unit entered is other than mentioned above
             }
-        }
-        else 
-        {
-            printf("Wrong unit entered.\n");    //If the unit entered is other than mentioned above
-        }
-        printf("To continue enter '1' to STOP enter '0': ");    //To continue or stop the loop
-        scanf("%d",&start);
-    }
-    if(start==0)
-    {
-        printf("\n\n--x--Thanks for using our TEMPERATURE CONVERTER!--x--");    //End of program
-    }
-    return 0;
 }
